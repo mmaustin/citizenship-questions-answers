0132868c42ca93b582298992906692807da577b0
McCray <tubach75@hotmail.com>
2022-01-22 12:06:45 -0500

    changed configure store to export default

--- citizenship-frontend/src/app/store.js
+++ citizenship-frontend/src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import questionsReducer from '../features/questions/questionsSlice'
 
-export const store = configureStore({
+export default configureStore({
   reducer: {
     questions: questionsReducer
   },
