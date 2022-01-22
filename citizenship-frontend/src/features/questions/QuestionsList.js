7df4ca26eccb11a57446ee4dd2f8dae806a7f8e5
McCray <tubach75@hotmail.com>
2022-01-22 13:18:27 -0500

    changed construction of questions array and mapped questions

--- citizenship-frontend/src/features/questions/QuestionsList.js
+++ citizenship-frontend/src/features/questions/QuestionsList.js
@@ -2,5 +2,13 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 export const QuestionsList = () => {
-    
+    const questions = useSelector(state => state.questions)
+    const questionsAnswers = questions.map(question => (
+        <article key={question.id}>
+            <p>{question.question}</p>
+            <p>{question.answer}</p>
+        </article>
+    ))
+
+
 }
--- citizenship-frontend/src/features/questions/questions.js
+++ citizenship-frontend/src/features/questions/questions.js
@@ -1,3 +1,3 @@
 import { nanoid } from '@reduxjs/toolkit'
 
-export const questions = [{id: nanoid(), question: "what is the supreme law of the land?", answer: {1: 'the constitution'}}]
+export const questions = [{id: nanoid(), question: "what is the supreme law of the land?", answer: 'the constitution'}]
