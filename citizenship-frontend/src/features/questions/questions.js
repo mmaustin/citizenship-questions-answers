b3af78f69d672d8c22333a9d862fd5719a64003c
McCray <tubach75@hotmail.com>
2022-01-23 12:25:51 -0500

    displaying multiple answers for a question

--- citizenship-frontend/src/features/questions/questions.js
+++ citizenship-frontend/src/features/questions/questions.js
@@ -1,3 +1,3 @@
 import { nanoid } from '@reduxjs/toolkit'
 
-export const questions = [{id: nanoid(), question: "what is the supreme law of the land?", answer: 'the constitution'}]
+export const questions = [{id: nanoid(), question: "what is the supreme law of the land?", answer: [{ans: 'the constitution'}, {ans: 'the other constitution'}]}]
