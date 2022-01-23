eedc9e085f57b82e0e33950ba3d933edb230d514
McCray <tubach75@hotmail.com>
2022-01-23 13:08:35 -0500

    added two additional question/answer pairs

--- citizenship-frontend/src/features/questions/questions.js
+++ citizenship-frontend/src/features/questions/questions.js
@@ -1,3 +1,7 @@
 import { nanoid } from '@reduxjs/toolkit'
 
-export const questions = [{id: nanoid(), question: "what is the supreme law of the land?", answer: [{ans: 'the constitution'}]}]
+export const questions = [{id: nanoid(), question: 'what is the supreme law of the land?', answer: [{ans: 'the constitution'}]},
+{id: nanoid(), question: 'what does the constitution do?', answer: [{ans: 'sets up the government'}, {ans: 'defines the government'},
+{ans: 'protects basic rights of americans'}]}, {id: nanoid(), question: 'the idea of self-government is in the first three words of the constitution. What are these words?',
+answer: [{ans: 'we the people'}]}
+]
