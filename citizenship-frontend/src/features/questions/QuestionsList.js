66080051f695678698c2068b4b50a9581acf1e81
McCray <tubach75@hotmail.com>
2022-01-23 13:52:43 -0500

    added class name attribute to article question container

--- citizenship-frontend/src/features/questions/QuestionsList.js
+++ citizenship-frontend/src/features/questions/QuestionsList.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux'
 export const QuestionsList = () => {
     const questions = useSelector(state => state.questions)
     const questionsAnswers = questions.map(question => (
-        <article key={question.id}>
+        <article className="question-excerpt" key={question.id}>
             <p>{question.question.substring(0, 100)}</p>
             {question.answer.map(a => (
                 <>
