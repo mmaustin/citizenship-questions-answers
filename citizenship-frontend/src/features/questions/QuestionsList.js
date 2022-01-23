e2b36bb6ee6c2e0033febcf8dba0e0c70a918c22
McCray <tubach75@hotmail.com>
2022-01-23 14:06:50 -0500

    added border to separate questions/answer pairs

--- citizenship-frontend/src/features/questions/QuestionsList.js
+++ citizenship-frontend/src/features/questions/QuestionsList.js
@@ -5,7 +5,7 @@ export const QuestionsList = () => {
     const questions = useSelector(state => state.questions)
     const questionsAnswers = questions.map(question => (
         <article className="question-excerpt" key={question.id}>
-            <p>{question.question.substring(0, 100)}</p>
+            <p>{question.question}</p>
             {question.answer.map(a => (
                 <>
                     <p>{a.ans}</p>
