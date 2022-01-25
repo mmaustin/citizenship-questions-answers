c24459535e947e6b27a2f9d4cd061aa59c468d41
McCray <tubach75@hotmail.com>
2022-01-25 11:41:59 -0500

    used use selector to retrieve a single question from state

--- citizenship-frontend/src/features/questions/SingleQuestionPage.js
+++ citizenship-frontend/src/features/questions/SingleQuestionPage.js
@@ -1,2 +1,11 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+
+export const SingleQuestionPage = ({match}) => {
+    const {questionId} = match.params
+    const question = useSelector(state => 
+        state.questions.find(question => question.id === questionId)
+    )
+
+    
+}
