8d0a3c2ae4f17fdef6f4c0de96bfb212ff2d2e72
McCray <tubach75@hotmail.com>
2022-01-29 12:12:47 -0500

    mapped questions const and added an index for options

--- citizenship-frontend/src/features/questions/QuestionsDropBox.js
+++ citizenship-frontend/src/features/questions/QuestionsDropBox.js
@@ -3,11 +3,16 @@ import { useSelector } from 'react-redux'
 
 export const QuestionsDropBox = () => {
     const questions = useSelector(state => state.questions)
+    const listQuestions = questions.map((q, idx) => (
+        <div key={idx}>
+            
+        </div>
+    ))
 
     return(
         <form>
             <select>
-                
+
             </select>
         </form>
     )
