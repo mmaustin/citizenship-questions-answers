ab8697ce9f6eac248940b117469ec789dbfe8ad8
McCray <tubach75@hotmail.com>
2022-01-29 12:04:19 -0500

    added from and select to handle options

--- citizenship-frontend/src/features/questions/QuestionsDropBox.js
+++ citizenship-frontend/src/features/questions/QuestionsDropBox.js
@@ -4,5 +4,12 @@ import { useSelector } from 'react-redux'
 export const QuestionsDropBox = () => {
     const questions = useSelector(state => state.questions)
 
+    return(
+        <form>
+            <select>
+                
+            </select>
+        </form>
+    )
 
 }
