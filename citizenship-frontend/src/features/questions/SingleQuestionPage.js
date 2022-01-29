d12e41f800ec547dcc76780d5a96cf149d00886b
McCray <tubach75@hotmail.com>
2022-01-29 13:12:19 -0500

    removed questions dropbox component from single q compoent

--- citizenship-frontend/src/features/questions/SingleQuestionPage.js
+++ citizenship-frontend/src/features/questions/SingleQuestionPage.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
-import { QuestionsDropBox } from './QuestionsDropBox'
 
 export const SingleQuestionPage = () => {
     //console.log({match})
@@ -32,7 +31,6 @@ export const SingleQuestionPage = () => {
                 <h3>{query.question}</h3>
                 {answers}
             </section>
-            <QuestionsDropBox/>
         </>
     )
 }
