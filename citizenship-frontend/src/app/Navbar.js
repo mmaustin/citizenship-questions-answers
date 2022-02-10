492c0e131367ec1ed5a6544ee42cad64d992bdbd
McCray <tubach75@hotmail.com>
2022-02-10 12:59:49 -0500

    added id's to nav sub-containers

--- citizenship-frontend/src/app/Navbar.js
+++ citizenship-frontend/src/app/Navbar.js
@@ -6,8 +6,8 @@ const Navbar = () => {
     return(
         <nav>
             <section id='nav-section'>
-                <h2>Citizenship: Questions, Answers, Quizzes</h2>
-                <div>
+                <h2 id='nav-heading'>Citizenship: Questions, Answers, Quizzes</h2>
+                <div id='nav-link-container'>
                     <div><Link to='/'>Quiz One Q & A's</Link></div>
                     <div><Link to='/questions/quizone'>Quiz One</Link></div>
                 </div>
