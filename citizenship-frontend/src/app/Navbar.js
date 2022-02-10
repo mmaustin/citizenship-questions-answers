bfde1ea321abe62995672ce4d80f23e35d9af188
McCray <tubach75@hotmail.com>
2022-02-10 13:19:33 -0500

    gave nav link container a flex flow of column

--- citizenship-frontend/src/app/Navbar.js
+++ citizenship-frontend/src/app/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     return(
         <nav>
             <section id='nav-section'>
-                <h2 id='nav-heading'>Citizenship: Questions, Answers, Quizzes</h2>
+                <h2 id='nav-heading'>Citizenship Test: Questions, Answers, Quizzes</h2>
                 <div id='nav-link-container'>
                     <div><Link to='/'>Quiz One Q & A's</Link></div>
                     <div><Link to='/questions/quizone'>Quiz One</Link></div>
