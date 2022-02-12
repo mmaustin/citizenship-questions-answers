f55a2f3a992531db0eeca4035602b5536b2bb94b
McCray <tubach75@hotmail.com>
2022-02-12 10:06:18 -0500

    experimenting with a/link tag styling

--- citizenship-frontend/src/app/Navbar.js
+++ citizenship-frontend/src/app/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
             <section id='nav-section'>
                 <h2 id='nav-heading'>Citizenship Test: Questions, Answers, Quizzes</h2>
                 <div id='nav-link-container'>
-                    <div><Link to='/'>Quiz One Q & A's</Link></div>
+                    <Link to='/'>Quiz One Q & A's</Link>
                     <div><Link to='/questions/quizone'>Quiz One</Link></div>
                 </div>
             </section>
