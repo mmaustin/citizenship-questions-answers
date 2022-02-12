660f93b97193a0823136d894f42e95b496d1f515
McCray <tubach75@hotmail.com>
2022-02-12 11:38:13 -0500

    gave the section housing the questions and answers a class name and border

--- citizenship-frontend/src/features/questions/QuestionsList.js
+++ citizenship-frontend/src/features/questions/QuestionsList.js
@@ -23,7 +23,7 @@ export const QuestionsList = () => {
     ))
 
     return(
-        <section>
+        <section className='question-answer-panel'>
             {questionsAnswers}
         </section>
     )
