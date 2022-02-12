502e025924ac000d17288f2258f0922ceb81aad3
McCray <tubach75@hotmail.com>
2022-02-12 11:06:44 -0500

    removed nanoid id from answers list, added question number using array index

--- citizenship-frontend/src/features/questions/QuestionsList.js
+++ citizenship-frontend/src/features/questions/QuestionsList.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { nanoid } from '@reduxjs/toolkit'
 import { Link } from 'react-router-dom'
 
 export const QuestionsList = () => {
     const questions = useSelector(state => state.questions)
     
-    const questionsAnswers = questions.map(question => (
+    const questionsAnswers = questions.map((question, idx) => (
         <article className="question-excerpt" key={question.id}>
+            <p>Question {idx + 1}</p>
             <p>{question.question}</p>
-            {question.answer.map(a => (
-                <div key={nanoid()}>
+            {question.answer.map((a, index) => (
+                <div key={index}>
                     <p>{a.ans}</p>
                 </div>
 
