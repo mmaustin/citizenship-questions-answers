2ca8fdb3d9d8a4a160c9fe1992db09b81c9f3ede
McCray <tubach75@hotmail.com>
2022-02-17 12:06:37 -0500

    added the last four questions and answers to the second quiz's array

--- citizenship-frontend/src/features/questionsTwo/questionsTwo.js
+++ citizenship-frontend/src/features/questionsTwo/questionsTwo.js
@@ -7,9 +7,10 @@ answer: [{ans: 'congress'}, {ans: 'legislative'}, {ans: 'president'}, {ans: 'exe
 {id: nanoid(), question: 'what stops one branch of government from becoming too powerful?', answer: [{ans: 'checks and balances'},
 {ans: 'separation of powers'}]}, {id: nanoid(), question: 'who is in charge of the executive branch?',
 answer: [{ans: 'the president'}]}, {id: nanoid(), question: 'who makes federal laws?', answer: [{ans: 'congress'},
-{ans: 'senate and house (of representatives)'}, {ans: '(u.s. or national) legislature'}]}, {id: nanoid(), question: 'how many amendments does the constitution have?',
-answer: [{ans: 'twenty-seven(27)'}]}, {id: nanoid(), question: 'what did the declaration of independence do?', answer: [{ans: 'announced independence from great britain'},
-{ans: 'declared independence from great britain'}, {ans: 'said that the united states is free from great britain'}]}, {id: nanoid(), question: 'what are two rights in the declaration of independence?',
-answer: [{ans: 'life'}, {ans: 'liberty'}, {ans: 'pursuit of happiness'}]}, {id: nanoid(), question: 'what is freedom of religion?',
-answer: [{ans: 'you can practice any religion or not practice a religion'}]}
+{ans: 'senate and house (of representatives)'}, {ans: '(u.s. or national) legislature'}]}, {id: nanoid(), question: 'what are the two parts of the u.s. congress?',
+answer: [{ans: 'the senate and the house of (representatives)'}]}, {id: nanoid(), question: 'how many u.s. senators are there?', answer: [{ans: 'one hundred (100)'}]}, 
+{id: nanoid(), question: 'we elect a u.s. senator for how many years?',
+answer: [{ans: 'six (6)'}]}, {id: nanoid(), question: 'the house of representatives has how many voting members?',
+answer: [{ans: 'four hundred thirty-five (435)'}]}
 ]
+//skipped question 20 from official list
