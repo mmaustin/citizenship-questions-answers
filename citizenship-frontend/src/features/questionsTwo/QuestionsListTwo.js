dc3cdb10db220b8a0fbc51eb9617f5fe5338d1fc
McCray <tubach75@hotmail.com>
2022-02-19 12:02:16 -0500

    copied, pasted, and configured code from questions list into questions list two component

---
+++ citizenship-frontend/src/features/questionsTwo/QuestionsListTwo.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+//import { Link } from 'react-router-dom'
+
+export const QuestionsList = () => {
+    const questions = useSelector(state => state.questionsTwo)
+    
+    const questionsAnswers = questions.map((question, idx) => (
+        <article className="question-excerpt" key={question.id}>
+            <p className='question-number'>Question {idx + 1}</p>
+            <p className='question'>{question.question}</p>
+            {question.answer.map((a, index) => (
+                <div className='answers-div' key={index}>
+                    <p className='answers'>{index +1}) {a.ans}</p>
+                </div>
+
+            ))}
+            
+            {/*<Link className='question-link' to={`/questions/${question.id}`}>
+            View Question
+            </Link> */}
+        </article>
+    ))
+
+    return(
+        <section className='question-answer-panel'>
+            {questionsAnswers}
+        </section>
+    )
+
+}
