c3f168d4e26c2ac7591617ab38a3ec2d48e9d0a7
McCray <tubach75@hotmail.com>
2022-02-19 11:16:31 -0500

    added quiz questions two slice to the store's reducer

--- citizenship-frontend/src/app/store.js
+++ citizenship-frontend/src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import questionsReducer from '../features/questions/questionsSlice'
 import quizQuestionsReducer from '../features/questions/quizQuestionsSlice'
 import questionsTwoReducer from '../features/questionsTwo/questionsSliceTwo';
+import quizQuestionsTwoReduceer from '../features/questionsTwo/quizQuestionsSliceTwo';
 
 export default configureStore({
   reducer: {
     questions: questionsReducer,
     quizQuestions: quizQuestionsReducer,
-    questionsTwo: questionsTwoReducer
+    questionsTwo: questionsTwoReducer,
+    quizQuestionsTwo: quizQuestionsTwoReduceer
   },
 });
