7190666a5bd9129cf800d0e21b6b9b0f920d1ef5
McCray <tubach75@hotmail.com>
2022-02-21 11:21:09 -0500

    added container div around q&a and quiz pairs

--- citizenship-frontend/src/app/Navbar.js
+++ citizenship-frontend/src/app/Navbar.js
@@ -8,10 +8,14 @@ const Navbar = () => {
             <section id='nav-section'>
                 <h2 id='nav-heading'>Citizenship Test Prep: Questions, Answers, Quizzes</h2>
                 <div id='nav-link-container'>
-                    <Link className='nav-link-text' to='/'>Quiz One Q & A's</Link>
-                    <Link className='nav-link-text' to='/questions/quiz-one'>Quiz One</Link>
-                    <Link className='nav-link-text' to='/questions/questions-two'>Quiz Two Q & A's</Link>
-                    <Link className='nav-link-text' to='/questions/quiz-two'>Quiz Two</Link>
+                    <div>
+                        <Link className='nav-link-text' to='/'>Quiz One Q & A's</Link>
+                        <Link className='nav-link-text' to='/questions/quiz-one'>Quiz One</Link>
+                    </div>
+                    <div>
+                        <Link className='nav-link-text' to='/questions/questions-two'>Quiz Two Q & A's</Link>
+                        <Link className='nav-link-text' to='/questions/quiz-two'>Quiz Two</Link>
+                    </div>
                 </div>
             </section>
         </nav>
