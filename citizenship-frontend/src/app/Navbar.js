3e2deec03d6e826c703c437a9a29265e38c3a23d
McCray <tubach75@hotmail.com>
2022-02-21 11:31:45 -0500

    gave quiz pair containers a class name

--- citizenship-frontend/src/app/Navbar.js
+++ citizenship-frontend/src/app/Navbar.js
@@ -8,11 +8,11 @@ const Navbar = () => {
             <section className='nav-section'>
                 <h2 className='nav-heading'>Citizenship Test Prep: Questions, Answers, Quizzes</h2>
                 <div className='nav-link-container'>
-                    <div >
+                    <div className='quiz-pair'>
                         <Link className='nav-link-text' to='/'>Quiz One Q & A's</Link>
                         <Link className='nav-link-text' to='/questions/quiz-one'>Quiz One</Link>
                     </div>
-                    <div>
+                    <div className='qiz-pair'>
                         <Link className='nav-link-text' to='/questions/questions-two'>Quiz Two Q & A's</Link>
                         <Link className='nav-link-text' to='/questions/quiz-two'>Quiz Two</Link>
                     </div>
