6df2ddba71ed66365c97b0098e09c27bffd12822
McCray <tubach75@hotmail.com>
2022-02-23 15:53:16 -0500

    shuffling order of the answers in the quiz answers two array

--- citizenship-frontend/src/features/questionsTwo/questionsTwo.js
+++ citizenship-frontend/src/features/questionsTwo/questionsTwo.js
@@ -7,15 +7,15 @@ answer: [{ans: 'congress'}, {ans: 'legislative'}, {ans: 'the president'}, {ans: 
 {id: nanoid(), question: 'what stops one branch of government from becoming too powerful?', answer: [{ans: 'checks and balances'},
 {ans: 'separation of powers'}]}, {id: nanoid(), question: 'who is in charge of the executive branch?',
 answer: [{ans: 'the president'}]}, {id: nanoid(), question: 'who makes federal laws?', answer: [{ans: 'congress'},
-{ans: 'senate and house (of representatives)'}, {ans: '(u.s. or national) legislature'}]}, {id: nanoid(), question: 'what are the two parts of the u.s. congress?',
-answer: [{ans: 'the senate and the house of (representatives)'}]}, {id: nanoid(), question: 'how many u.s. senators are there?', answer: [{ans: 'one hundred (100)'}]}, 
+{ans: 'the senate and the house of representatives'}, {ans: '(u.s. or national) legislature'}]}, {id: nanoid(), question: 'what are the two parts of the u.s. congress?',
+answer: [{ans: 'the senate and the house of representatives'}]}, {id: nanoid(), question: 'how many u.s. senators are there?', answer: [{ans: 'one hundred (100)'}]}, 
 {id: nanoid(), question: 'we elect a u.s. senator for how many years?',
 answer: [{ans: 'six (6)'}]}, {id: nanoid(), question: 'the house of representatives has how many voting members?',
 answer: [{ans: 'four hundred thirty-five (435)'}]}
 ]
 //skipped question 20 from official list
 
-export const quizAnswersTwo = ['capitalist economy', 'market economy', 'everyone must follow the law', 'leaders must obey the law', 'government must obey the law',
-'no one is above the law', 'congress', 'legislative', 'the president', 'executive', 'the courts', 'judicial', 'checks and balances', 'separation of powers',
-'congress', 'senate and house (of representatives)', '(u.s. or national) legislature', 'the senate and the house of (representatives)', 'one hundred (100)',
+export const quizAnswersTwo = ['the courts', 'the senate and the house of representatives', '(u.s. or national) legislature', 'leaders must obey the law', 'government must obey the law',
+'no one is above the law', 'congress', 'one hundred (100)', 'the president', 'executive', 'capitalist economy', 'judicial', 'checks and balances', 'separation of powers',
+'congress', 'everyone must follow the law', 'market economy', 'legislative',
 'six (6)', 'four hundred thirty-five (435)']
