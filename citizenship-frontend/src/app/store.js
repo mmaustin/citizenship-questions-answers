279aa87442dec75935ab1f41bf04331861e1ccc7
McCray <tubach75@hotmail.com>
2022-02-24 09:36:15 -0500

    added the display slice reducer to the redux store

--- citizenship-frontend/src/app/store.js
+++ citizenship-frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import quizQuestionsReducer from '../features/questions/quizQuestionsSlice'
+import displaySliceReducer from '../features/questions/displaySlice';
 import quizQuestionsTwoReduceer from '../features/questionsTwo/quizQuestionsSliceTwo';
 
 export default configureStore({
   reducer: {
     quizQuestions: quizQuestionsReducer,
+    displayQuestions: displaySliceReducer,
     quizQuestionsTwo: quizQuestionsTwoReduceer
   },
 });
--- citizenship-frontend/src/features/questions/displaySlice.js
+++ citizenship-frontend/src/features/questions/displaySlice.js
@@ -1,3 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { questionAnswerDisplay } from "./questions";
 
+const initialState = questionAnswerDisplay
+  
+  const displaySlice = createSlice({
+    name: 'displayQuestions',
+    initialState,
+    reducers: {}
+  })
+  
+  //export const {removeQuestion} = quizQuestionsSlice.actions
+
+  export default displaySlice.reducer
