c024f2a826137c77958867734e4c77d3e6a1052e
McCray <tubach75@hotmail.com>
2022-02-26 10:58:19 -0500

    removed number of question from the display

--- citizenship-frontend/src/features/questions/QuizOne.js
+++ citizenship-frontend/src/features/questions/QuizOne.js
@@ -16,8 +16,8 @@ export const QuizOne = () => {
 
     const displayArray = display.map((q, idx) => (
         q.value === 'Correct'
-           ? <p className='right' key={idx}>{idx +1}: {q.question}: {q.displayAnswer}: {q.value}</p>
-           : <p className='wrong' key={idx}>{idx +1}: {q.question}: {q.displayAnswer}: {q.value}</p>
+           ? <p className='right' key={idx}>{q.question}: {q.displayAnswer}: {q.value}</p>
+           : <p className='wrong' key={idx}>{q.question}: {q.displayAnswer}: {q.value}</p>
     ))
     
 
