b8244853b558b065e4edda41e85bb0376292c92f
McCray <tubach75@hotmail.com>
2022-02-26 12:06:27 -0500

    configuring code to display question answers

--- citizenship-frontend/src/features/questions/QuizOne.js
+++ citizenship-frontend/src/features/questions/QuizOne.js
@@ -15,17 +15,17 @@ export const QuizOne = () => {
     const dispatch = useDispatch()
 
     const displayArray = display.map((q, idx) => (
-        /*if (q.value === 'Correct'){
+        //if (q.value === 'Correct'){
         <article>
             <p key={idx}>{q.question}: {q.displayAnswer}: <span className='right'>{q.value}</span></p>
             {q.answer.map(a => (
                 <p>{a.ans}</p>
             ))}
         </article>
-        }*/
-        q.value === 'Correct'
+        //}
+        /*q.value === 'Correct'
            ? <p key={idx}>{q.question}: {q.displayAnswer}: <span className='right'>{q.value}</span></p>
-           : <p key={idx}>{q.question}: {q.displayAnswer}: <span className='wrong'>{q.value}</span></p>
+           : <p key={idx}>{q.question}: {q.displayAnswer}: <span className='wrong'>{q.value}</span></p>*/
     ))
     
 
