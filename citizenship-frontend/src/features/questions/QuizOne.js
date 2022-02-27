007db83e8ff86e60af126c7b48e32b37739bb06f
McCray <tubach75@hotmail.com>
2022-02-27 14:08:57 -0500

    added width and borders to wrong answers display container

--- citizenship-frontend/src/features/questions/QuizOne.js
+++ citizenship-frontend/src/features/questions/QuizOne.js
@@ -17,15 +17,15 @@ export const QuizOne = () => {
     const displayArray = display.map((q, idx) => (
         q.value === 'Correct'
         ? <>
-            <p className='display-right-answer' key={idx}><span className='right'>Question:</span> {q.question} <span className='right'>Your Answer:</span> {q.displayAnswer} <span className='right'>Result: {q.value}</span></p>
-            <div className='correct-answers-container'>
-                {/*{q.answer.map((a, idx) => (
+            <p className='display-right-answer' key={idx}>{q.question} <span className='right'>Your Answer:</span> {q.displayAnswer} <span className='right'>{q.value}</span></p>
+            {/*<div className='correct-answers-container'>
+                {q.answer.map((a, idx) => (
                     <p className='correct-answers' key={idx}>{a.ans}</p>
-                ))}*/}
-            </div>    
+                ))}
+            </div>*/}
         </>
         : <>
-            <p className='display-wrong-answer' key={idx}><span className='wrong'>Question:</span> {q.question} <span className='wrong'>Your Answer:</span> {q.displayAnswer} <span className='wrong'>Result: {q.value}</span></p>
+            <p className='display-wrong-answer' key={idx}>{q.question} <span className='wrong'>Your Answer:</span> {q.displayAnswer} <span className='wrong'>{q.value}</span></p>
             <div className='wrong-answers-container'>
                 {q.answer.map(a => (
                     <p>{a.ans}</p>
