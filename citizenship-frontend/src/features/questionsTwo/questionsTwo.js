8b2e8be99553b20c8c547aa8491b527ec636e506
McCray <tubach75@hotmail.com>
2022-03-01 15:29:53 -0500

    added display answer and value keys to second quiz display array

--- citizenship-frontend/src/features/questionsTwo/questionsTwo.js
+++ citizenship-frontend/src/features/questionsTwo/questionsTwo.js
@@ -13,22 +13,22 @@ answer: [{ans: 'the senate and the house of representatives'}]}, {id: nanoid(), 
 answer: [{ans: 'six (6)'}]}, {id: nanoid(), question: 'the house of representatives has how many voting members?',
 answer: [{ans: 'four hundred thirty-five (435)'}]}
 ]
-//skipped question 20 from official list displayAnswer: 'Pending', value: ''}
+//skipped question 20 from official list 
 
 export const quizAnswersTwo = ['the courts', 'the senate and the house of representatives', 'leaders must obey the law', 'checks and balances',
 'no one is above the law', 'congress', 'one hundred (100)', 'six (6)', 'executive', 'capitalist economy', 'judicial', 'government must obey the law', 'separation of powers',
 'congress', 'four hundred thirty-five (435)', 'market economy', 'legislative',
 'the president', 'everyone must follow the law', '(u.s. or national) legislature']
 
-export const questionAnswerDisplayTwo = [{id: nanoid(), question: 'what is the economic system in the united states?', answer: [{ans: 'capitalist economy'}, {ans: 'market economy'}]},
-{id: nanoid(), question: 'what is the \'rule of law\'?', answer: [{ans: 'everyone must follow the law'}, {ans: 'leaders must obey the law'},
-{ans: 'government must obey the law'}, {ans: 'no one is above the law'}]}, {id: nanoid(), question: 'name one branch or part of the government?',
-answer: [{ans: 'congress'}, {ans: 'legislative'}, {ans: 'the president'}, {ans: 'executive'}, {ans: 'the courts'}, {ans: 'judicial'}]}, 
+export const questionAnswerDisplayTwo = [{id: nanoid(), question: 'what is the economic system in the united states?', answer: [{ans: 'capitalist economy'}, {ans: 'market economy'}], 
+displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'what is the \'rule of law\'?', answer: [{ans: 'everyone must follow the law'}, {ans: 'leaders must obey the law'},
+{ans: 'government must obey the law'}, {ans: 'no one is above the law'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'name one branch or part of the government?',
+answer: [{ans: 'congress'}, {ans: 'legislative'}, {ans: 'the president'}, {ans: 'executive'}, {ans: 'the courts'}, {ans: 'judicial'}], displayAnswer: 'Pending', value: ''}, 
 {id: nanoid(), question: 'what stops one branch of government from becoming too powerful?', answer: [{ans: 'checks and balances'},
-{ans: 'separation of powers'}]}, {id: nanoid(), question: 'who is in charge of the executive branch?',
-answer: [{ans: 'the president'}]}, {id: nanoid(), question: 'who makes federal laws?', answer: [{ans: 'congress'},
-{ans: 'the senate and the house of representatives'}, {ans: '(u.s. or national) legislature'}]}, {id: nanoid(), question: 'what are the two parts of the u.s. congress?',
-answer: [{ans: 'the senate and the house of representatives'}]}, {id: nanoid(), question: 'how many u.s. senators are there?', answer: [{ans: 'one hundred (100)'}]}, 
+{ans: 'separation of powers'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'who is in charge of the executive branch?',
+answer: [{ans: 'the president'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'who makes federal laws?', answer: [{ans: 'congress'},
+{ans: 'the senate and the house of representatives'}, {ans: '(u.s. or national) legislature'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'what are the two parts of the u.s. congress?',
+answer: [{ans: 'the senate and the house of representatives'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'how many u.s. senators are there?', answer: [{ans: 'one hundred (100)'}], displayAnswer: 'Pending', value: ''}, 
 {id: nanoid(), question: 'we elect a u.s. senator for how many years?',
-answer: [{ans: 'six (6)'}]}, {id: nanoid(), question: 'the house of representatives has how many voting members?',
-answer: [{ans: 'four hundred thirty-five (435)'}]}]
+answer: [{ans: 'six (6)'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'the house of representatives has how many voting members?',
+answer: [{ans: 'four hundred thirty-five (435)'}], displayAnswer: 'Pending', value: ''}]
