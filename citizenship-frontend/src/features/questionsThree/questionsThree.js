c2f83fc72c4735245aba9e9481b96b27d249ee99
McCray <tubach75@hotmail.com>
2022-03-02 11:20:23 -0500

    added the  next three questions and answers to questions three array

--- citizenship-frontend/src/features/questionsThree/questionsThree.js
+++ citizenship-frontend/src/features/questionsThree/questionsThree.js
@@ -3,13 +3,12 @@ import { nanoid } from "@reduxjs/toolkit"
 export const questionsThree = [{id: nanoid(), question: 'we elect a u.s. representative for how many years?', answer: [{ans: 'two (2)'}]},
 {id: nanoid(), question: 'who does a u.s. senator represent?', answer: [{ans: 'all people of the state'}]}, {id: nanoid(), question: 'why do some states have more representatives than other states?',
 answer: [{ans: 'because of the state\'s population'}, {ans: 'because they have more people'}, {ans: 'because some states have more people'}]}, 
-{id: nanoid(), question: 'what stops one branch of government from becoming too powerful?', answer: [{ans: 'checks and balances'},
-{ans: 'separation of powers'}]}, {id: nanoid(), question: 'who is in charge of the executive branch?',
-answer: [{ans: 'the president'}]}, {id: nanoid(), question: 'who makes federal laws?', answer: [{ans: 'congress'},
-{ans: 'the senate and the house of representatives'}, {ans: '(u.s. or national) legislature'}]}, {id: nanoid(), question: 'what are the two parts of the u.s. congress?',
+{id: nanoid(), question: 'we elect a president for how many years?', answer: [{ans: 'four (4)'}]}, {id: nanoid(), question: 'in what month do we vote for president?',
+answer: [{ans: 'november'}]}, {id: nanoid(), question: 'if the president can no longer serve, who becomes president?', answer: [{ans: 'the vice president'}]}, 
+{id: nanoid(), question: 'what are the two parts of the u.s. congress?',
 answer: [{ans: 'the senate and the house of representatives'}]}, {id: nanoid(), question: 'how many u.s. senators are there?', answer: [{ans: 'one hundred (100)'}]}, 
 {id: nanoid(), question: 'we elect a u.s. senator for how many years?',
 answer: [{ans: 'six (6)'}]}, {id: nanoid(), question: 'the house of representatives has how many voting members?',
 answer: [{ans: 'four hundred thirty-five (435)'}]}]
 
-//skipped questions 23
+//skipped questions 23, 28, 29
