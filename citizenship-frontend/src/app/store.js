bf7e375f13950805b20ae641dfbe9cc7bac46cb3
McCray <tubach75@hotmail.com>
2022-03-02 12:28:30 -0500

    added slice three to the store

--- citizenship-frontend/src/app/store.js
+++ citizenship-frontend/src/app/store.js
@@ -3,12 +3,14 @@ import quizQuestionsReducer from '../features/questions/quizQuestionsSlice'
 import displaySliceReducer from '../features/questions/displaySlice';
 import quizQuestionsReducerTwo from '../features/questionsTwo/quizQuestionsSliceTwo';
 import displaySliceReducerTwo from '../features/questionsTwo/displaySliceTwo';
+import quizQuestionsReducerThree from '../features/questionsThree/quizQuestionsSliceThree';
 
 export default configureStore({
   reducer: {
     quizQuestions: quizQuestionsReducer,
     displayQuestions: displaySliceReducer,
     quizQuestionsTwo: quizQuestionsReducerTwo,
-    displayQuestionsTwo: displaySliceReducerTwo
+    displayQuestionsTwo: displaySliceReducerTwo,
+    quizQuestionsThree: quizQuestionsReducerThree
   },
 });
