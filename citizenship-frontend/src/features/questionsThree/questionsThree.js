209951001360c07116f27d1bbf2796d1c7ea7de7
McCray <tubach75@hotmail.com>
2022-03-02 11:25:55 -0500

    added the last four questions to questions three array

--- citizenship-frontend/src/features/questionsThree/questionsThree.js
+++ citizenship-frontend/src/features/questionsThree/questionsThree.js
@@ -5,10 +5,10 @@ export const questionsThree = [{id: nanoid(), question: 'we elect a u.s. represe
 answer: [{ans: 'because of the state\'s population'}, {ans: 'because they have more people'}, {ans: 'because some states have more people'}]}, 
 {id: nanoid(), question: 'we elect a president for how many years?', answer: [{ans: 'four (4)'}]}, {id: nanoid(), question: 'in what month do we vote for president?',
 answer: [{ans: 'november'}]}, {id: nanoid(), question: 'if the president can no longer serve, who becomes president?', answer: [{ans: 'the vice president'}]}, 
-{id: nanoid(), question: 'what are the two parts of the u.s. congress?',
-answer: [{ans: 'the senate and the house of representatives'}]}, {id: nanoid(), question: 'how many u.s. senators are there?', answer: [{ans: 'one hundred (100)'}]}, 
-{id: nanoid(), question: 'we elect a u.s. senator for how many years?',
-answer: [{ans: 'six (6)'}]}, {id: nanoid(), question: 'the house of representatives has how many voting members?',
-answer: [{ans: 'four hundred thirty-five (435)'}]}]
+{id: nanoid(), question: 'if both the president and the vice president can no longer serve, who becomes president??',
+answer: [{ans: 'the speaker of the house'}]}, {id: nanoid(), question: 'who is the commander in chief of the military?', answer: [{ans: 'the president'}]}, 
+{id: nanoid(), question: 'who signs bills to become laws?',
+answer: [{ans: 'the president'}]}, {id: nanoid(), question: 'who vetoes bills?',
+answer: [{ans: 'the president'}]}]
 
 //skipped questions 23, 28, 29
