eb21a9e610890c0425ecc39a208eabcc26982e51
McCray <tubach75@hotmail.com>
2022-03-03 16:19:47 -0500

    added route to questions list four to app component

--- citizenship-frontend/src/App.js
+++ citizenship-frontend/src/App.js
@@ -7,6 +7,7 @@ import { QuestionsListTwo } from './features/questionsTwo/QuestionsListTwo'
 import { QuizTwo } from './features/questionsTwo/QuizTwo'
 import { QuestionsListThree } from './features/questionsThree/QuestionsListThree'
 import { QuizThree } from './features/questionsThree/QuizThree'
+import { QuestionsListFour } from './features/questionsFour/QuestionsListFour'
 
 const App = () => {
 
@@ -21,6 +22,8 @@ const App = () => {
                     <Route exact path='/questions/quiz-two' element={<QuizTwo/>}/>
                     <Route exact path='/questions/questions-three' element={<QuestionsListThree/>}/>
                     <Route exact path='/questions/quiz-three' element={<QuizThree/>}/>
+                    <Route exact path='/questions/questions-four' element={<QuestionsListFour/>}/>
+                    {/*<Route exact path='/questions/quiz-three' element={<QuizThree/>}/>*/}
                     <Route path="*" element={<p>There's Nothing At This Address!</p> }/>
                 </Routes>
         </Router>
