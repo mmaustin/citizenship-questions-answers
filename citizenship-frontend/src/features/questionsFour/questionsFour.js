49be57d3ed7dbb1e0f4ff685d73c714786eeea14
McCray <tubach75@hotmail.com>
2022-03-03 15:54:33 -0500

    added the next three questions to the questions array

--- citizenship-frontend/src/features/questionsFour/questionsFour.js
+++ citizenship-frontend/src/features/questionsFour/questionsFour.js
@@ -3,8 +3,8 @@ import { nanoid } from "@reduxjs/toolkit";
 export const questionsFour = [{id: nanoid(), question: 'what does the president\'s Cabinet do?', answer: [{ans: 'advises the president'}]},
 {id: nanoid(), question: 'what does the judicial branch do?', answer: [{ans: 'reviews laws'}, {ans: 'explains laws'}, {ans: 'resolves disputes (disagreements)'}, {ans: 'decides if a law goes against the constitution'}]},
 {id: nanoid(), question: 'what is the highest court in the united states?', answer: [{ans: 'the supreme court'}]}, 
-{id: nanoid(), question: 'we elect a president for how many years?', answer: [{ans: 'four (4)'}]}, {id: nanoid(), question: 'in what month do we vote for president?',
-answer: [{ans: 'november'}]}, {id: nanoid(), question: 'if the president can no longer serve, who becomes president?', answer: [{ans: 'the vice president'}]}, 
+{id: nanoid(), question: 'what is the name of the president of the united states now?', answer: [{ans: 'joe biden'}]}, {id: nanoid(), question: 'what is the name of the vice president of the united states now?',
+answer: [{ans: 'Kamala Harris'}]}, {id: nanoid(), question: 'how many justices are on the supreme court?', answer: [{ans: 'nine (9)'}]}, 
 {id: nanoid(), question: 'if both the president and the vice president can no longer serve, who becomes president?',
 answer: [{ans: 'the speaker of the house'}]}, {id: nanoid(), question: 'who is the commander in chief of the military?', answer: [{ans: 'the president'}]}, 
 {id: nanoid(), question: 'who signs bills to become laws?',
@@ -12,3 +12,4 @@ answer: [{ans: 'the president'}]}, {id: nanoid(), question: 'who vetoes bills?',
 answer: [{ans: 'the president'}]}]
 
 //skipped question 36
+//added questions 28, 29
