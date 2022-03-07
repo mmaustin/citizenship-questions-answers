2182632d7dc11387f3035440e4d4197ac1031925
McCray <tubach75@hotmail.com>
2022-03-07 10:20:12 -0500

    completed question six questions array

--- citizenship-frontend/src/features/questionsSix/questionsSix.js
+++ citizenship-frontend/src/features/questionsSix/questionsSix.js
@@ -5,9 +5,12 @@ export const questionsSix = [{id: nanoid(), question: 'what is one reason coloni
 {id: nanoid(), question: 'what group of people was taken to america and sold as slaves?', answer: [{ans: 'africans'}, {ans: 'people from africa'}]}, 
 {id: nanoid(), question: 'why did the colonists fight the british?', answer: [{ans: 'because of high taxes (taxation without representation)'},
 {ans: 'because the british army stayed in their houses (boarding, quartering)'}, {ans: 'because they didn\'t have self-government'}]}, {id: nanoid(), question: 'who wrote the declaration of independence?',
-answer: [{ans: 'thomas jefferson'}]}, {id: nanoid(), question: 'if the president can no longer serve, who becomes president?', answer: [{ans: 'the vice president'}]}, 
-{id: nanoid(), question: 'if both the president and the vice president can no longer serve, who becomes president?',
-answer: [{ans: 'the speaker of the house'}]}, {id: nanoid(), question: 'who is the commander in chief of the military?', answer: [{ans: 'the president'}]}, 
-{id: nanoid(), question: 'who signs bills to become laws?',
-answer: [{ans: 'the president'}]}, {id: nanoid(), question: 'who vetoes bills?',
-answer: [{ans: 'the president'}]}]
+answer: [{ans: 'thomas jefferson'}]}, {id: nanoid(), question: 'when was the declaration of independence adopted', answer: [{ans: 'july 4, 1776'}]}, 
+{id: nanoid(), question: 'what happened at the constitutional convention?',
+answer: [{ans: 'the constitution was written'}, {ans: 'the founding fathers wrote the constitution'}]}, {id: nanoid(), question: 'when was the constitution written?', answer: [{ans: '1787'}]}, 
+{id: nanoid(), question: 'the federalists papers support the passage of the u.s. constitution. name one of the writers',
+answer: [{ans: 'james madison'}, {ans: 'alexander hamilton'}, {ans: 'john jay'}, {ans: 'publius'}]}, {id: nanoid(), question: 'what is one thing benjamin franklin is famous for?',
+answer: [{ans: 'u.s diplomat'}, {ans: 'oldest member of the constitutional convention'}, {ans: 'first postmaster general of the united states'}, 
+{ans: 'writer of "poor richard\'s almanac"'}, {ans: 'started the first free libraries'}]}]
+
+//skipped question 64
