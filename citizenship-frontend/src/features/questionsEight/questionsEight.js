853163b03d532e41b49df5a96e8bfd3f1662dd2c
McCray <tubach75@hotmail.com>
2022-03-09 13:20:50 -0500

    added display answer and value keys to qa display eight array

--- citizenship-frontend/src/features/questionsEight/questionsEight.js
+++ citizenship-frontend/src/features/questionsEight/questionsEight.js
@@ -17,18 +17,16 @@ export const quizAnswersEight = ['mississippi river', 'woodrow wilson', 'civil r
 'world war II', 'communism', 'worked for equality for all americans',
 'franklin roosevelt', 'terrorists attacked the united states', 'pacific ocean',
 'japan, germany, and italy', 'fought for civil rights', 'missouri river']
-//, displayAnswer: 'Pending', value: ''
 
-export const questionAnswerDisplayEight = [{id: nanoid(), question: 'who is the "father of our country"?', answer: [{ans: 'george washington'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'who was the first president?',
-answer: [{ans: 'george washington'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'what territory did the u.s. buy from france in 1803?', answer: [{ans: 'the louisiana territory'}, {ans: 'louisiana'}], displayAnswer: 'Pending', value: ''}, 
-{id: nanoid(), question: 'name one war fought by the united states in the 1800s', answer: [{ans: 'war of 1812'},
-{ans: 'mexican-american war'}, {ans: 'the civil war'}, {ans: 'spanish-american war'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'name the u.s. war between the north and the south',
-answer: [{ans: 'the civil war'}, {ans: 'the war between the states'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'name one problem that led to the civil war', answer: [{ans: 'slavery'}, {ans: 'economic reasons'}, {ans: 'states\' rights'}], displayAnswer: 'Pending', value: ''}, 
-{id: nanoid(), question: 'what was one important thing that abraham lincoln did?',
-answer: [{ans: 'freed the slaves (emancipation proclamation)'}, {ans: 'saved or preserved the union'}, {ans: 'led the united states during the civil war'}], displayAnswer: 'Pending', value: ''},
-{id: nanoid(), question: 'what did the emancipation proclamation do?', answer: [{ans: 'freed the slaves'}, {ans: 'freed slaves in the confederacy'}, {ans: 'freed slaves in the confederate states'}, {ans: 'freed slaves in most southern states'}], displayAnswer: 'Pending', value: ''}, 
-{id: nanoid(), question: 'what did susan b. anthony do?',
-answer: [{ans: 'fought for women\'s rights'}, {ans: 'fought for civil rights'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'name one war fought by the united states in the 1900s.',
-answer: [{ans: 'world war I'}, {ans: 'world war II'}, {ans: 'the korean war'}, 
-{ans: 'the vietnam war'}, {ans: 'persian gulf war'}], displayAnswer: 'Pending', value: ''}]
+
+export const questionAnswerDisplayEight = [{id: nanoid(), question: 'who was president during world war I?', answer: [{ans: 'woodrow wilson'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'who was president during the great drepression and world war II?',
+answer: [{ans: 'franklin roosevelt'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'who did the united states fight in world war II?', answer: [{ans: 'japan, germany, and italy'}], displayAnswer: 'Pending', value: ''}, 
+{id: nanoid(), question: 'before he was president, eisenhower was a general. what war was he in?', answer: [{ans: 'world war II'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'during the cold war, what was the main concern of the united states?',
+answer: [{ans: 'communism'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'what movement tried to end racial discrimination', answer: [{ans: 'civil rights movement'}], displayAnswer: 'Pending', value: ''}, 
+{id: nanoid(), question: 'what did martin luther king, jr. do?',
+answer: [{ans: 'fought for civil rights'}, {ans: 'worked for equality for all americans'}], displayAnswer: 'Pending', value: ''},
+{id: nanoid(), question: 'what major event happened on september 11, 2001, in the united states?', answer: [{ans: 'terrorists attacked the united states'}], displayAnswer: 'Pending', value: ''}, 
+{id: nanoid(), question: 'name one of the two longest rivers in the united states',
+answer: [{ans: 'missouri river'}, {ans: 'mississippi river'}], displayAnswer: 'Pending', value: ''}, {id: nanoid(), question: 'was ocean is on the west coast of the united states?',
+answer: [{ans: 'pacific ocean'}], displayAnswer: 'Pending', value: ''}]
 
