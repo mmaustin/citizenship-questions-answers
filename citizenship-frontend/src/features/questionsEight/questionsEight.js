822ee33884c62b1dcf96199c69ba86585366b993
McCray <tubach75@hotmail.com>
2022-03-09 13:07:24 -0500

    completed the questions eight array

--- citizenship-frontend/src/features/questionsEight/questionsEight.js
+++ citizenship-frontend/src/features/questionsEight/questionsEight.js
@@ -1,19 +1,17 @@
 import { nanoid } from "@reduxjs/toolkit";
 
-export const questionsEight = [{id: nanoid(), question: 'who is the "father of our country"?', answer: [{ans: 'george washington'}]}, {id: nanoid(), question: 'who was the first president?',
-answer: [{ans: 'george washington'}]}, {id: nanoid(), question: 'what territory did the u.s. buy from france in 1803?', answer: [{ans: 'the louisiana territory'}, {ans: 'louisiana'}]}, 
-{id: nanoid(), question: 'name one war fought by the united states in the 1800s', answer: [{ans: 'war of 1812'},
-{ans: 'mexican-american war'}, {ans: 'the civil war'}, {ans: 'spanish-american war'}]}, {id: nanoid(), question: 'name the u.s. war between the north and the south',
-answer: [{ans: 'the civil war'}, {ans: 'the war between the states'}]}, {id: nanoid(), question: 'name one problem that led to the civil war', answer: [{ans: 'slavery'}, {ans: 'economic reasons'}, {ans: 'states\' rights'}]}, 
-{id: nanoid(), question: 'what was one important thing that abraham lincoln did?',
-answer: [{ans: 'freed the slaves (emancipation proclamation)'}, {ans: 'saved or preserved the union'}, {ans: 'led the united states during the civil war'}]},
-{id: nanoid(), question: 'what did the emancipation proclamation do?', answer: [{ans: 'freed the slaves'}, {ans: 'freed slaves in the confederacy'}, {ans: 'freed slaves in the confederate states'}, {ans: 'freed slaves in most southern states'}]}, 
-{id: nanoid(), question: 'what did susan b. anthony do?',
-answer: [{ans: 'fought for women\'s rights'}, {ans: 'fought for civil rights'}]}, {id: nanoid(), question: 'name one war fought by the united states in the 1900s.',
-answer: [{ans: 'world war I'}, {ans: 'world war II'}, {ans: 'the korean war'}, 
-{ans: 'the vietnam war'}, {ans: 'persian gulf war'}]}]
+export const questionsEight = [{id: nanoid(), question: 'who was president during world war I?', answer: [{ans: 'woodrow wilson'}]}, {id: nanoid(), question: 'who was president during the great drepression and world war II?',
+answer: [{ans: 'franklin roosevelt'}]}, {id: nanoid(), question: 'who did the united states fight in world war II?', answer: [{ans: 'japan, germany, and italy'}]}, 
+{id: nanoid(), question: 'before he was president, eisenhower was a general. what war was he in?', answer: [{ans: 'world war II'}]}, {id: nanoid(), question: 'during the cold war, what was the main concern of the united states?',
+answer: [{ans: 'communism'}]}, {id: nanoid(), question: 'what movement tried to end racial discrimination', answer: [{ans: 'civil rights movement'}]}, 
+{id: nanoid(), question: 'what did martin luther king, jr. do?',
+answer: [{ans: 'fought for civil rights'}, {ans: 'worked for equality for all americans'}]},
+{id: nanoid(), question: 'what major event happened on september 11, 2001, in the united states?', answer: [{ans: 'terrorists attacked the united states'}]}, 
+{id: nanoid(), question: 'name one of the two longest rivers in the united states',
+answer: [{ans: 'missouri river'}, {ans: 'mississippi river'}]}, {id: nanoid(), question: 'was ocean is on the west coast of the united states?',
+answer: [{ans: 'pacific ocean'}]}]
 
-//skipped question 64
+//skipped question 87
 
 export const quizAnswersEight = ['persian gulf war', 'freed slaves in most southern states', 'world war I', 'fought for women\'s rights', 'slavery',
 'mexican-american war', 'freed slaves in the confederacy', 'the korean war', 'the louisiana territory', 'the civil war', 'economic reasons', 'saved or preserved the union',
