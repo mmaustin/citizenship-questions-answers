a358add549161322737a10710a1054b5e8a6627d
McCray <tubach75@hotmail.com>
2022-03-10 13:03:50 -0500

    added quiz nine component and configured copied quiz eight code

---
+++ citizenship-frontend/src/features/questionsNine/QuizNine.js
@@ -0,0 +1,132 @@
+import React, {useState} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeQuestion } from './quizQuestionsSliceNine'
+import { quizAnswersNine } from './questionsNine'
+import { addAnswer } from './displaySliceNine'
+
+export const QuizNine = () => {
+    const questions = useSelector(state => state.quizQuestionsNine)
+    const displayNine = useSelector(state => state.displayQuestionsNine)
+    const [question, setQuestion] = useState('');
+    const [selectAnswer, setSelectAnswer] = useState('')
+    const [correctAnswer, setCorrectAnswer] = useState(0)
+    const [incorrectAnswer, setIncorrectAnswer] = useState(0)
+
+    const dispatch = useDispatch()
+
+    const displayArray = displayNine.map((q, idx) => (
+        q.value === 'Correct'
+        ? <div className='ternary-container'>
+            <p className='display-right-answer' key={idx}>{q.question.charAt(0).toUpperCase() + q.question.slice(1)} <span className='right'>Your Answer:</span> {q.displayAnswer.charAt(0).toUpperCase() + q.displayAnswer.slice(1)} &nbsp;<span className='right'>{q.value}</span></p>
+            {/*<div className='correct-answers-container'>
+                {q.answer.map((a, idx) => (
+                    <p className='correct-answers' key={idx}>{a.ans}</p>
+                ))}
+            </div>*/}
+        </div>
+        : <div className='ternary-container'>
+            <p className='display-wrong-answer' key={idx}>{q.question.charAt(0).toUpperCase() + q.question.slice(1)} <span className='wrong'>Your Answer:</span> {q.displayAnswer.charAt(0).toUpperCase() + q.displayAnswer.slice(1)} &nbsp;<span className='wrong'>{q.value}</span></p>
+            <div className='wrong-answers-container'>
+                {q.answer.map((a, idx) => (
+                    <p key={idx} className='quiz-answers-display'>• {a.ans}</p>
+                ))}
+            </div>
+        </div>
+    ))
+
+    const listQuestions = questions.map(q => (
+        q.question
+    ))
+
+
+
+    const getRandomQuestion = () => {
+        if (listQuestions.length !== 0){
+            setQuestion(listQuestions[Math.floor(Math.random() * listQuestions.length)]);
+        }
+    }
+
+    const removeSelectedQuestion = () => {
+        dispatch(removeQuestion(question))
+    }
+
+    const answers = quizAnswersNine.map((answer, idx) => (
+            <option key={idx} value={answer}>{answer}</option>
+    ))
+
+    const handleChange = (e) => {
+        setSelectAnswer(e.target.value)
+    }
+
+    const displayAddAnswer = () => {
+        dispatch(addAnswer({question, selectAnswer}))
+    }
+
+    const tabulation = () => {
+        const ques = questions.find(q => q.question === question)
+        const q = ques.answer.map(a => a.ans)
+        if(q.includes(selectAnswer)){
+            setCorrectAnswer(correctAnswer + 1)
+        } else {
+            setIncorrectAnswer(incorrectAnswer + 1)
+        }
+    }
+
+    const clearAnswerField = () => {
+        setSelectAnswer('')
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        tabulation()
+
+        removeSelectedQuestion()
+        displayAddAnswer()
+        clearAnswerField()
+    }
+
+    const questionsAnswered = correctAnswer + incorrectAnswer
+
+    const allowSubmit = Boolean(selectAnswer)
+
+    return(
+        <div className='quiz-container'>
+            <div className='sub-container-one'>
+                <h3 className='pass-requirement'>You must correctly answer 7 out of 10 questions to pass this quiz.</h3>
+                <p className='questions-answered'>Questions Answered: {questionsAnswered}</p>
+                <p className='selected-question'>{question.toUpperCase()}</p>
+                <button className='question-selector' onClick={getRandomQuestion}>Click to Select a Question</button>
+            </div>
+            <div className='sub-container-two'>
+                <form onSubmit={e => handleSubmit(e)}>
+                    <label>
+                        Select Your Answer:&nbsp;&nbsp;&nbsp;
+                        <select className='answer-selector' value={selectAnswer} onChange={e => handleChange(e)}>
+                            <option value=""></option>
+                            {answers}
+                        </select>
+                    </label>&nbsp;&nbsp;&nbsp;
+                    <input className='submit-answer' type="submit" value="Submit" disabled={!allowSubmit}/>
+                </form>
+            </div>    
+            {listQuestions.length === 0 &&
+            <>
+                <div className='sub-container-three'>
+                    <h2 className='results-header'>RESULTS:</h2>
+                    <p className='answer-count'>Correct Answers: {correctAnswer}</p>
+                    <p className='answer-count'>Incorrect Answers: {incorrectAnswer}</p>
+                    {correctAnswer >= 7 
+                        ? <h3 className='result-statement'>Congratulations! You Passed!!</h3>
+                        : <h3 className='result-statement'>Don't fret! Keep studying and practicing!</h3>
+                    }
+                </div>
+                <div className='sub-container-four'>
+                    {displayArray}
+                </div>
+            </>
+            }
+        </div>
+    )
+
+}
