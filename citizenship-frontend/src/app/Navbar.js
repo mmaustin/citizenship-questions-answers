94842e27d529bf3632fa0008db420e366608892f
McCray <tubach75@hotmail.com>
2022-03-12 12:24:21 -0500

    removed the dropdown menu, as the individual links look better

--- citizenship-frontend/src/app/Navbar.js
+++ citizenship-frontend/src/app/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
 
     return(
         <nav>
-            {/*<section className='nav-section'>
+            <section className='nav-section'>
                 <h2 className='nav-heading'>Citizenship Test Prep: Questions, Answers, Quizzes</h2>
                 <div className='nav-link-container'>
                     <div className='quiz-pair'>
@@ -45,56 +45,7 @@ const Navbar = () => {
                         <Link className='nav-link-text' to='/questions/quiz-nine'>Quiz Nine</Link>
                     </div>
                 </div>
-            </section>*/}
-            <div class="dropdown">
-                <button class="dropbtn">Dropdown</button>
-                <div class="dropdown-content">
-                <div className='quiz-pair'>
-                        <Link className='nav-link-text' to='/'>Quiz One Q&A</Link>
-                        <Link className='nav-link-text' to='/questions/quiz-one'>Quiz One</Link>
-                    </div>
-                    <div className='quiz-pair'>
-                        <Link className='nav-link-text' to='/questions/questions-two'>Quiz Two Q&A</Link>
-                        <Link className='nav-link-text' to='/questions/quiz-two'>Quiz Two</Link>
-                    </div>
-                    <div className='quiz-pair'>
-                        <Link className='nav-link-text' to='/questions/questions-three'>Quiz Three Q&A</Link>
-                        <Link className='nav-link-text' to='/questions/quiz-three'>Quiz Three</Link>
-                    </div>
-                    <div className='quiz-pair'>
-                        <Link className='nav-link-text' to='/questions/questions-four'>Quiz Four Q&A</Link>
-                        <Link className='nav-link-text' to='/questions/quiz-four'>Quiz Four</Link>
-                    </div>
-                    <div className='quiz-pair'>
-                        <Link className='nav-link-text' to='/questions/questions-five'>Quiz Five Q&A</Link>
-                        <Link className='nav-link-text' to='/questions/quiz-five'>Quiz Five</Link>
-                    </div>
-                    <div className='quiz-pair'>
-                        <Link className='nav-link-text' to='/questions/questions-six'>Quiz Six Q&A</Link>
-                        <Link className='nav-link-text' to='/questions/quiz-six'>Quiz Six</Link>
-                    </div>
-                    <div className='quiz-pair'>
-                        <Link className='nav-link-text' to='/questions/questions-seven'>Quiz Seven Q&A</Link>
-                        <Link className='nav-link-text' to='/questions/quiz-seven'>Quiz Seven</Link>
-                    </div>
-                    <div className='quiz-pair'>
-                        <Link className='nav-link-text' to='/questions/questions-eight'>Quiz Eight Q&A</Link>
-                        <Link className='nav-link-text' to='/questions/quiz-eight'>Quiz Eight</Link>
-                    </div>
-                    <div className='quiz-pair'>
-                        <Link className='nav-link-text' to='/questions/questions-nine'>Quiz Nine Q&A</Link>
-                        <Link className='nav-link-text' to='/questions/quiz-nine'>Quiz Nine</Link>
-                    </div>
-                    {/*<Link className='nav-link-text' to='/'>Quiz One Q&A</Link>
-                    <Link className='nav-link-text' to='/questions/quiz-one'>Quiz One</Link>
-                    <Link className='nav-link-text' to='/questions/questions-two'>Quiz Two Q&A</Link>
-                    <Link className='nav-link-text' to='/questions/quiz-two'>Quiz Two</Link>
-                    <Link className='nav-link-text' to='/questions/questions-three'>Quiz Three Q&A</Link>
-                    <Link className='nav-link-text' to='/questions/quiz-three'>Quiz Three</Link>
-                    <Link className='nav-link-text' to='/questions/questions-four'>Quiz Four Q&A</Link>
-                    <Link className='nav-link-text' to='/questions/quiz-four'>Quiz Four</Link>*/}
-                </div>
-            </div>
+            </section>
         </nav>
     )
 
