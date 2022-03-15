0f4d12bc8142c66967d694ec9fe618ebbeb68e2e
McCray <tubach75@hotmail.com>
2022-03-15 09:43:44 -0400

    wrote code to set up an array full of checked boxes boolean values

--- citizenship-frontend/src/features/questions/PracticeComponent.js
+++ citizenship-frontend/src/features/questions/PracticeComponent.js
@@ -2,5 +2,7 @@ import React, {useState} from 'react'
 import { quizAnswers } from './questions'
 
 export const PracticeComponent = () => {
-    
+    const [checkedState, setCheckedState] = useState(
+        new Array(quizAnswers.length).fill(false)
+    )
 }
