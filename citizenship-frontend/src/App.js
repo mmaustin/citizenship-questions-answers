13843f0f085e288b2e53743c5941a797fcbf2785
McCray <tubach75@hotmail.com>
2022-03-22 15:44:49 -0400

    removed the initial /questions from routes in app component

--- citizenship-frontend/src/App.js
+++ citizenship-frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { HomePage } from './features/homePage/HomePage'
-import { QuestionsList } from './features/questions/QuestionsList'
+import { QuestionsList } from './features/QuestionsList'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Navbar from './app/Navbar'
-import { QuizOne } from './features/questions/QuizOne'
+import { QuizOne } from './features/QuizOne'
 import { QuestionsListTwo } from './features/questionsTwo/QuestionsListTwo'
 import { QuizTwo } from './features/questionsTwo/QuizTwo'
 import { QuestionsListThree } from './features/questionsThree/QuestionsListThree'
@@ -30,22 +30,22 @@ const App = () => {
                     <Route exact path='/' element={<HomePage/>}/>
                     <Route exact path='/questions-one' element={<QuestionsList/>}/>
                     <Route exact path='/quiz-one' element={<QuizOne/>}/>
-                    <Route exact path='/questions/questions-two' element={<QuestionsListTwo/>}/>
-                    <Route exact path='/questions/quiz-two' element={<QuizTwo/>}/>
-                    <Route exact path='/questions/questions-three' element={<QuestionsListThree/>}/>
-                    <Route exact path='/questions/quiz-three' element={<QuizThree/>}/>
-                    <Route exact path='/questions/questions-four' element={<QuestionsListFour/>}/>
-                    <Route exact path='/questions/quiz-four' element={<QuizFour/>}/>
-                    <Route exact path='/questions/questions-five' element={<QuestionsListFive/>}/>
-                    <Route exact path='/questions/quiz-five' element={<QuizFive/>}/>
-                    <Route exact path='/questions/questions-six' element={<QuestionsListSix/>}/>
-                    <Route exact path='/questions/quiz-six' element={<QuizSix/>}/>
-                    <Route exact path='/questions/questions-seven' element={<QuestionsListSeven/>}/>
-                    <Route exact path='/questions/quiz-seven' element={<QuizSeven/>}/>
-                    <Route exact path='/questions/questions-eight' element={<QuestionsListEight/>}/>
-                    <Route exact path='/questions/quiz-eight' element={<QuizEight/>}/>
-                    <Route exact path='/questions/questions-nine' element={<QuestionsListNine/>}/>
-                    <Route exact path='/questions/quiz-nine' element={<QuizNine/>}/>
+                    <Route exact path='/questions-two' element={<QuestionsListTwo/>}/>
+                    <Route exact path='/quiz-two' element={<QuizTwo/>}/>
+                    <Route exact path='/questions-three' element={<QuestionsListThree/>}/>
+                    <Route exact path='/quiz-three' element={<QuizThree/>}/>
+                    <Route exact path='/questions-four' element={<QuestionsListFour/>}/>
+                    <Route exact path='/quiz-four' element={<QuizFour/>}/>
+                    <Route exact path='/questions-five' element={<QuestionsListFive/>}/>
+                    <Route exact path='/quiz-five' element={<QuizFive/>}/>
+                    <Route exact path='/questions-six' element={<QuestionsListSix/>}/>
+                    <Route exact path='/quiz-six' element={<QuizSix/>}/>
+                    <Route exact path='/questions-seven' element={<QuestionsListSeven/>}/>
+                    <Route exact path='/quiz-seven' element={<QuizSeven/>}/>
+                    <Route exact path='/questions-eight' element={<QuestionsListEight/>}/>
+                    <Route exact path='/quiz-eight' element={<QuizEight/>}/>
+                    <Route exact path='/questions-nine' element={<QuestionsListNine/>}/>
+                    <Route exact path='/quiz-nine' element={<QuizNine/>}/>
                     <Route path="*" element={<p>There's Nothing At This Address!</p> }/>
                 </Routes>
         </Router>
