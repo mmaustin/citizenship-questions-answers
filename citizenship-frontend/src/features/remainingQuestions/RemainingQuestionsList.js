8b8f5b4a1a4c4f2e63ccee9a2235b5c5c08327da
McCray <tubach75@hotmail.com>
2022-03-22 16:32:01 -0400

    pasted code to display the remaining questions

---
+++ citizenship-frontend/src/features/remainingQuestions/RemainingQuestionsList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { questionsTen } from './questionsTen'
+
+export const RemainingQuestionsList = () => {
+    
+    const questionsAnswers = questionsTen.map((question, idx) => (
+        <article className="question-excerpt" key={question.id}>
+            <p className='question-number'>Question {idx + 1}</p>
+            <p className='question'>{question.question}</p>
+            {question.answer.map((a, index) => (
+                <div className='answers-div' key={index}>
+                    <p className='answers'>{index +1}) {a.ans}</p>
+                </div>
+
+            ))}
+        </article>
+    ))
+
+    return(
+        <section className='question-answer-panel'>
+            {questionsAnswers}
+        </section>
+    )
+
+}
