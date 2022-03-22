ed025325b05478f4e338d72c17323a1776459a1f
McCray <tubach75@hotmail.com>
2022-03-22 12:49:58 -0400

    added non conditional link to homepage in navbar component

--- citizenship-frontend/src/app/Navbar.js
+++ citizenship-frontend/src/app/Navbar.js
@@ -8,6 +8,9 @@ const Navbar = () => {
             <section className='nav-section'>
                 <h2 className='nav-heading'>Citizenship Test Prep: Questions, Answers, Quizzes</h2>
                 <div className='nav-link-container'>
+                <div className='quiz-pair'>
+                        <Link className='nav-link-text' to='/'>Home Page</Link>
+                    </div>
                     <div className='quiz-pair'>
                         <Link className='nav-link-text' to='/questions-one'>Quiz One Q&A</Link>
                         <Link className='nav-link-text' to='/quiz-one'>Quiz One</Link>
