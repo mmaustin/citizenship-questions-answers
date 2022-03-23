6333c2ffd3793dd447d84e1615640025c8995db3
McCray <tubach75@hotmail.com>
2022-03-23 15:54:41 -0400

    removed commented code from the quiz two components

--- citizenship-frontend/src/features/questionsTwo/displaySliceTwo.js
+++ citizenship-frontend/src/features/questionsTwo/displaySliceTwo.js
@@ -1,31 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { questionAnswerDisplayTwo } from "./questionsTwo";
 
-/*const initialState = questionAnswerDisplayTwo
-  
-  const displaySliceTwo = createSlice({
-    name: 'displayQuestionsTwo',
-    initialState,
-    reducers: {
-        addAnswer(state, action){
-            const {question, selectAnswer} = action.payload
-            const foundQuestion = state.find(q => q.question === question)
-            const foundQuestionAnswers = foundQuestion.answer.map(q => q.ans)
-            if (foundQuestionAnswers.includes(selectAnswer)){
-                foundQuestion.displayAnswer = selectAnswer;
-                foundQuestion.value = 'Correct';
-            } else {
-                foundQuestion.displayAnswer = selectAnswer;
-                foundQuestion.value = 'Incorrect';
-            }
-        }
-    }
-  })
-  
-  export const {addAnswer} = displaySliceTwo.actions
-
-  export default displaySliceTwo.reducer*/
-
   const initialState = questionAnswerDisplayTwo
   
   const displaySliceTwo = createSlice({
