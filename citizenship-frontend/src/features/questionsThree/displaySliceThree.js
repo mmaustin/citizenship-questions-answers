896364d382387885ddc674e52e463d0c7c33d081
McCray <tubach75@hotmail.com>
2022-03-23 16:16:49 -0400

    removed commented code from the quiz three components

--- citizenship-frontend/src/features/questionsThree/displaySliceThree.js
+++ citizenship-frontend/src/features/questionsThree/displaySliceThree.js
@@ -1,31 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { questionAnswerDisplayThree } from "./questionsThree";
 
-/*const initialState = questionAnswerDisplayThree
-  
-  const displaySliceThree = createSlice({
-    name: 'displayQuestionsThree',
-    initialState,
-    reducers: {
-        addAnswer(state, action){
-            const {question, selectAnswer} = action.payload
-            const foundQuestion = state.find(q => q.question === question)
-            const foundQuestionAnswers = foundQuestion.answer.map(q => q.ans)
-            if (foundQuestionAnswers.includes(selectAnswer)){
-                foundQuestion.displayAnswer = selectAnswer;
-                foundQuestion.value = 'Correct';
-            } else {
-                foundQuestion.displayAnswer = selectAnswer;
-                foundQuestion.value = 'Incorrect';
-            }
-        }
-    }
-  })
-  
-  export const {addAnswer} = displaySliceThree.actions
-
-  export default displaySliceThree.reducer*/
-
   const initialState = questionAnswerDisplayThree
   
   const displaySliceThree = createSlice({
