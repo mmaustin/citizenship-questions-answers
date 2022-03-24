b9964667b96839ceb05933c782002ad7c7d6e2aa
McCray <tubach75@hotmail.com>
2022-03-24 16:18:48 -0400

    added copied code to handle multiple answers in slice fsix component

--- citizenship-frontend/src/features/questionsSix/displaySliceSix.js
+++ citizenship-frontend/src/features/questionsSix/displaySliceSix.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { questionAnswerDisplaySix } from "./questionsSix";
 
-const initialState = questionAnswerDisplaySix
+/*const initialState = questionAnswerDisplaySix
   
   const displaySliceSix = createSlice({
     name: 'displayQuestionsSix',
@@ -17,6 +17,44 @@ const initialState = questionAnswerDisplaySix
             } else {
                 foundQuestion.displayAnswer = selectAnswer;
                 foundQuestion.value = 'Incorrect';
+            }
+        }
+    }
+  })
+  
+  export const {addAnswer} = displaySliceSix.actions
+
+  export default displaySliceSix.reducer*/
+
+  const initialState = questionAnswerDisplaySix
+  
+  const displaySliceSix = createSlice({
+    name: 'displayQuestionsSix',
+    initialState,
+    reducers: {
+        addAnswer(state, action){
+            const {question, answersHolder} = action.payload
+            const foundQuestion = state.find(q => q.question === question)
+            const foundQuestionAnswers = foundQuestion.answer.map(q => q.ans)
+            if(answersHolder.length > 1){
+                const multiAnswers = answersHolder.map(answer => {
+                    return foundQuestionAnswers.includes(answer)
+                })
+                if(multiAnswers.includes(false)){
+                    foundQuestion.displayAnswer = answersHolder.join(' / ');
+                    foundQuestion.value = 'Incorrect';
+                } else {
+                    foundQuestion.displayAnswer = answersHolder.join(' / ');
+                    foundQuestion.value = 'Correct';
+                }
+            } else {
+                if (foundQuestionAnswers.includes(answersHolder[0])){
+                    foundQuestion.displayAnswer = answersHolder[0];
+                    foundQuestion.value = 'Correct';
+                } else {
+                    foundQuestion.displayAnswer = answersHolder[0];
+                    foundQuestion.value = 'Incorrect';
+                }
             }
         }
     }
