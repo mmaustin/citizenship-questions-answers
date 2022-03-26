4b211537eb91451bc336846073a950bfdbea57bb
McCray <tubach75@hotmail.com>
2022-03-26 15:10:59 -0400

    deleted commented code from quiz eight components

--- citizenship-frontend/src/features/questionsEight/displaySliceEight.js
+++ citizenship-frontend/src/features/questionsEight/displaySliceEight.js
@@ -1,22 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { questionAnswerDisplayEight } from "./questionsEight";
 
-const initialState = questionAnswerDisplayEight
+  const initialState = questionAnswerDisplayEight
   
   const displaySliceEight = createSlice({
     name: 'displayQuestionsEight',
     initialState,
     reducers: {
         addAnswer(state, action){
-            const {question, selectAnswer} = action.payload
+            const {question, answersHolder} = action.payload
             const foundQuestion = state.find(q => q.question === question)
             const foundQuestionAnswers = foundQuestion.answer.map(q => q.ans)
-            if (foundQuestionAnswers.includes(selectAnswer)){
-                foundQuestion.displayAnswer = selectAnswer;
-                foundQuestion.value = 'Correct';
+            if(answersHolder.length > 1){
+                const multiAnswers = answersHolder.map(answer => {
+                    return foundQuestionAnswers.includes(answer)
+                })
+                if(multiAnswers.includes(false)){
+                    foundQuestion.displayAnswer = answersHolder.join(' / ');
+                    foundQuestion.value = 'Incorrect';
+                } else {
+                    foundQuestion.displayAnswer = answersHolder.join(' / ');
+                    foundQuestion.value = 'Correct';
+                }
             } else {
-                foundQuestion.displayAnswer = selectAnswer;
-                foundQuestion.value = 'Incorrect';
+                if (foundQuestionAnswers.includes(answersHolder[0])){
+                    foundQuestion.displayAnswer = answersHolder[0];
+                    foundQuestion.value = 'Correct';
+                } else {
+                    foundQuestion.displayAnswer = answersHolder[0];
+                    foundQuestion.value = 'Incorrect';
+                }
             }
         }
     }
@@ -24,4 +37,4 @@ const initialState = questionAnswerDisplayEight
   
   export const {addAnswer} = displaySliceEight.actions
 
-  export default displaySliceEight.reducer
+  export default displaySliceEight.reducer  
