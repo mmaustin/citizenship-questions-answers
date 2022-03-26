e5096ed61a5bf59e57fc4ee724f12b874499fe00
McCray <tubach75@hotmail.com>
2022-03-26 15:16:34 -0400

    amended slice nine to handle multiple answers

--- citizenship-frontend/src/features/questionsNine/displaySliceNine.js
+++ citizenship-frontend/src/features/questionsNine/displaySliceNine.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { questionAnswerDisplayNine } from "./questionsNine";
 
-const initialState = questionAnswerDisplayNine
+/*const initialState = questionAnswerDisplayNine
   
   const displaySliceNine = createSlice({
     name: 'displayQuestionsNine',
@@ -24,4 +24,42 @@ const initialState = questionAnswerDisplayNine
   
   export const {addAnswer} = displaySliceNine.actions
 
-  export default displaySliceNine.reducer
+  export default displaySliceNine.reducer*/
+
+  const initialState = questionAnswerDisplayNine
+  
+  const displaySliceNine = createSlice({
+    name: 'displayQuestionsNine',
+    initialState,
+    reducers: {
+        addAnswer(state, action){
+            const {question, answersHolder} = action.payload
+            const foundQuestion = state.find(q => q.question === question)
+            const foundQuestionAnswers = foundQuestion.answer.map(q => q.ans)
+            if(answersHolder.length > 1){
+                const multiAnswers = answersHolder.map(answer => {
+                    return foundQuestionAnswers.includes(answer)
+                })
+                if(multiAnswers.includes(false)){
+                    foundQuestion.displayAnswer = answersHolder.join(' / ');
+                    foundQuestion.value = 'Incorrect';
+                } else {
+                    foundQuestion.displayAnswer = answersHolder.join(' / ');
+                    foundQuestion.value = 'Correct';
+                }
+            } else {
+                if (foundQuestionAnswers.includes(answersHolder[0])){
+                    foundQuestion.displayAnswer = answersHolder[0];
+                    foundQuestion.value = 'Correct';
+                } else {
+                    foundQuestion.displayAnswer = answersHolder[0];
+                    foundQuestion.value = 'Incorrect';
+                }
+            }
+        }
+    }
+  })
+  
+  export const {addAnswer} = displaySliceNine.actions
+
+  export default displaySliceNine.reducer  
