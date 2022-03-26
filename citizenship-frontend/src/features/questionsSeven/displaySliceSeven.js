9fe01308999b00795b56258e1d31135104830a60
McCray <tubach75@hotmail.com>
2022-03-26 15:00:25 -0400

    removed commented code from quiz seven components

--- citizenship-frontend/src/features/questionsSeven/displaySliceSeven.js
+++ citizenship-frontend/src/features/questionsSeven/displaySliceSeven.js
@@ -1,31 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { questionAnswerDisplaySeven } from "./questionsSeven";
 
-/*const initialState = questionAnswerDisplaySeven
-  
-  const displaySliceSeven = createSlice({
-    name: 'displayQuestionsSeven',
-    initialState,
-    reducers: {
-        addAnswer(state, action){
-            const {question, selectAnswer} = action.payload
-            const foundQuestion = state.find(q => q.question === question)
-            const foundQuestionAnswers = foundQuestion.answer.map(q => q.ans)
-            if (foundQuestionAnswers.includes(selectAnswer)){
-                foundQuestion.displayAnswer = selectAnswer;
-                foundQuestion.value = 'Correct';
-            } else {
-                foundQuestion.displayAnswer = selectAnswer;
-                foundQuestion.value = 'Incorrect';
-            }
-        }
-    }
-  })
-  
-  export const {addAnswer} = displaySliceSeven.actions
-
-  export default displaySliceSeven.reducer*/
-
   const initialState = questionAnswerDisplaySeven
   
   const displaySliceSeven = createSlice({
