04919d8a70adde363315ad203f345dcacc507f8a
McCray <tubach75@hotmail.com>
2022-03-27 11:46:08 -0400

    added the first three questions and answers to questions ten array

--- citizenship-frontend/src/features/remainingQuestions/questionsTen.js
+++ citizenship-frontend/src/features/remainingQuestions/questionsTen.js
@@ -1,8 +1,10 @@
 import { nanoid } from "@reduxjs/toolkit";
 
-export const questionsTen = [{id: nanoid(), question: 'who was president during world war I?', answer: [{ans: 'woodrow wilson'}]}, {id: nanoid(), question: 'who was president during the great drepression and world war II?',
-answer: [{ans: 'franklin roosevelt'}]}, {id: nanoid(), question: 'who did the united states fight in world war II?', answer: [{ans: 'japan, germany, and italy'}]}, 
-{id: nanoid(), question: 'before he was president, eisenhower was a general. what war was he in?', answer: [{ans: 'world war II'}]}, {id: nanoid(), question: 'during the cold war, what was the main concern of the united states?',
+export const questionsTen = [{id: nanoid(), question: 'what is one of your state\'s u.s. senators now?', answer: [{ans: 'answers will vary'}]}, {id: nanoid(), question: 'name your u.s. representative',
+answer: [{ans: 'answers will vary'}]}, {id: nanoid(), question: 'what are two cabinet-level positions?', answer: [{ans: 'secretary of agriculture'}, {ans: 'secretary of commerce'},
+{ans: 'secretary of defense'}, {ans: 'secretary of education'}, {ans: 'secretary of energy'}, {ans: 'secretary of health and human services'},
+{ans: 'secretary of homeland security'}, {ans: 'secretary of housing and urban development'}, {ans: 'secretary of the interior'}, {ans: 'secretary of labor'}, {ans: 'secretary of state'}, {ans: 'secretary of transportation'},
+{ans: 'secretary of the treasury'}, {ans: 'secretary of veterans affairs'}, {ans: 'the attorney general'}, {ans: 'the vice president'}]}, {id: nanoid(), question: 'during the cold war, what was the main concern of the united states?',
 answer: [{ans: 'communism'}]}, {id: nanoid(), question: 'what movement tried to end racial discrimination', answer: [{ans: 'civil rights movement'}]}, 
 {id: nanoid(), question: 'what did martin luther king, jr. do?',
 answer: [{ans: 'fought for civil rights'}, {ans: 'worked for equality for all americans'}]},
