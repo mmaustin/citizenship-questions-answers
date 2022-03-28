7f3a1e4b85479994a631cc4b7c84028c1b51745a
McCray <tubach75@hotmail.com>
2022-03-28 15:57:45 -0400

    added links to the provided dropdown code

--- citizenship-frontend/src/app/Navbar.js
+++ citizenship-frontend/src/app/Navbar.js
@@ -60,9 +60,28 @@ const Navbar = () => {
             <div class="dropdown">
                 <button class="dropbtn">Dropdown</button>
                 <div class="dropdown-content">
-                    
+                    <Link className='nav-link-text' to='/'>Home Page</Link>
+                    <Link className='nav-link-text' to='/questions-one'>Quiz One Q&A</Link>
+                    <Link className='nav-link-text' to='/quiz-one'>Quiz One</Link>
+                    <Link className='nav-link-text' to='/questions-two'>Quiz Two Q&A</Link>
+                    <Link className='nav-link-text' to='/quiz-two'>Quiz Two</Link>
+                    <Link className='nav-link-text' to='/questions-three'>Quiz Three Q&A</Link>
+                    <Link className='nav-link-text' to='/quiz-three'>Quiz Three</Link>
+                    <Link className='nav-link-text' to='/questions-four'>Quiz Four Q&A</Link>
+                    <Link className='nav-link-text' to='/quiz-four'>Quiz Four</Link>
+                    <Link className='nav-link-text' to='/questions-five'>Quiz Five Q&A</Link>
+                    <Link className='nav-link-text' to='/quiz-five'>Quiz Five</Link>
+                    <Link className='nav-link-text' to='/questions-six'>Quiz Six Q&A</Link>
+                    <Link className='nav-link-text' to='/quiz-six'>Quiz Six</Link>
+                    <Link className='nav-link-text' to='/questions-seven'>Quiz Seven Q&A</Link>
+                    <Link className='nav-link-text' to='/quiz-seven'>Quiz Seven</Link>
+                    <Link className='nav-link-text' to='/questions-eight'>Quiz Eight Q&A</Link>
+                    <Link className='nav-link-text' to='/quiz-eight'>Quiz Eight</Link>
+                    <Link className='nav-link-text' to='/questions-nine'>Quiz Nine Q&A</Link>
+                    <Link className='nav-link-text' to='/quiz-nine'>Quiz Nine</Link>
+                    <Link className='nav-link-text' to='/remaining-questions'>More Questions</Link>
                 </div>
-            </div>
+            </div>    
         </nav>
     )
 
