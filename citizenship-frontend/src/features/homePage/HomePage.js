b87b78c29b1dbefb6a4a8d41558547eec262565e
McCray <tubach75@hotmail.com>
2022-04-01 10:01:16 -0400

    added dummy data to homepage elements for help with presentation

--- citizenship-frontend/src/features/homePage/HomePage.js
+++ citizenship-frontend/src/features/homePage/HomePage.js
@@ -4,10 +4,11 @@ export const HomePage = () => {
     return(
         <div id='home-container'>
             <div id='home-welcome'>
-
+                <h3>Become A United States Citizen</h3>
+                <p>ffff</p>
             </div>
             <div id='home-instructions'>
-
+                <p>ggg</p>
             </div>
         </div>
     )
