34aa3584a1434bf247bfc23ef8434473f3b19f29
McCray <tubach75@hotmail.com>
2022-04-04 16:45:40 -0400

    added to insturctions and other info on the homepage

--- citizenship-frontend/src/features/homePage/HomePage.js
+++ citizenship-frontend/src/features/homePage/HomePage.js
@@ -11,12 +11,16 @@ export const HomePage = () => {
                 </h4>
             </div>
             <div id='home-instructions'>
-                <h4>Instructions</h4>
+                <h3>Instructions and other useful information:</h3>
                 <p>This app contains an updated list of the hundred questions and answers that comprise
                     the citizenship test. All questions and answers are taken from the U.S. citizenship
-                    and Immigration Services (USCIS) website. 
+                    and Immigration Services (USCIS) website. There are nine Q&A/Quiz pairs, plus a stand
+                    alone page of questions for which there is not a quiz. Hover over the green menu button
+                    in the upper left-hand corner to access the Q&A pages and quizzes. Study the questions
+                    and answers. Then, click on the associated quiz. It is that simple! (It's suggested that
+                    you attempt Quiz One first, in order to view the chronology of the quiz.)
                 </p>
-                
+
             </div>
         </div>
     )
