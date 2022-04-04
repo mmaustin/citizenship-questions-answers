cbe3c65fe44aa5b28c1b58b145466ddfac4233ec
McCray <tubach75@hotmail.com>
2022-04-04 16:28:21 -0400

    commenced the instructions section of the homepage

--- citizenship-frontend/src/features/homePage/HomePage.js
+++ citizenship-frontend/src/features/homePage/HomePage.js
@@ -5,13 +5,18 @@ export const HomePage = () => {
         <div id='home-container'>
             <div id='home-welcome'>
                 <h3>Welcome! Are You Ready To Become A United States Citizen!</h3>
-                <p>You will soon be a participant in one of the greatest experiments in the world.
-                   You don't have to take my word for it.  Once you're a citizen, you will be able
+                <h4>You will soon be a participant in one of the greatest experiments in the world.
+                   You don't have to take my word for it. Once you're a citizen, you will be able
                    to see for yourself! Let's get started.
-                </p>
+                </h4>
             </div>
             <div id='home-instructions'>
-                <p>gggg</p>
+                <h4>Instructions</h4>
+                <p>This app contains an updated list of the hundred questions and answers that comprise
+                    the citizenship test. All questions and answers are taken from the U.S. citizenship
+                    and Immigration Services (USCIS) website. 
+                </p>
+                
             </div>
         </div>
     )
