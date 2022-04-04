6321210274f1261ec3712c5dac7ff85e93ca67af
McCray <tubach75@hotmail.com>
2022-04-04 16:16:44 -0400

    added more commentary to the homepage

--- citizenship-frontend/src/features/homePage/HomePage.js
+++ citizenship-frontend/src/features/homePage/HomePage.js
@@ -5,7 +5,10 @@ export const HomePage = () => {
         <div id='home-container'>
             <div id='home-welcome'>
                 <h3>Welcome! Are You Ready To Become A United States Citizen!</h3>
-                <p>ffff</p>
+                <p>You will soon be a participant in one of the greatest experiments in the world.
+                   You don't have to take my word for it.  Once you're a citizen, you will be able
+                   to see for yourself! Let's get started.
+                </p>
             </div>
             <div id='home-instructions'>
                 <p>gggg</p>
