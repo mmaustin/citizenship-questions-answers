7552fb59958d003787aee9e50603ad74a9b40e1c
McCray <tubach75@hotmail.com>
2022-04-28 15:41:24 -0400

    changed the welcome container's banner messages

--- citizenship-frontend/src/features/homePage/HomePage.js
+++ citizenship-frontend/src/features/homePage/HomePage.js
@@ -4,10 +4,8 @@ export const HomePage = () => {
     return(
         <div id='home-container'>
             <div id='home-welcome'>
-                <h3>Welcome! Are You Ready To Become A United States Citizen!</h3>
-                <h4>You will soon be a participant in one of the greatest experiments in the world.
-                   You don't have to take my word for it. Once you're a citizen, you will be able
-                   to see for yourself! Let's get started.
+                <h3>Welcome! Your Pathway To Citizenship Begins Here!</h3>
+                <h4>Let's get started.
                 </h4>
             </div>
             <div id='home-instructions'>
