980a179e80a0f50a8d429ea3a0af111ebbe3c05d
McCray <tubach75@hotmail.com>
2022-04-28 15:45:29 -0400

    added id's to elements in home-welcome container

--- citizenship-frontend/src/features/homePage/HomePage.js
+++ citizenship-frontend/src/features/homePage/HomePage.js
@@ -4,8 +4,8 @@ export const HomePage = () => {
     return(
         <div id='home-container'>
             <div id='home-welcome'>
-                <h3>Welcome! Your Pathway To Citizenship Begins Here!</h3>
-                <h4>Let's get started.
+                <h3 id='welcome'>Welcome! Your Pathway To Citizenship Begins Here!</h3>
+                <h4 id='welcome-start'>Let's Get Started!
                 </h4>
             </div>
             <div id='home-instructions'>
