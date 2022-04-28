4ec7324f638ddf7d236f4bebf0c037ebc7fdb626
McCray <tubach75@hotmail.com>
2022-04-28 15:52:07 -0400

    added ul/li elements and class/id's to instructions container

--- citizenship-frontend/src/features/homePage/HomePage.js
+++ citizenship-frontend/src/features/homePage/HomePage.js
@@ -9,15 +9,10 @@ export const HomePage = () => {
                 </h4>
             </div>
             <div id='home-instructions'>
-                <h3>Instructions and other useful information:</h3>
-                <p>This app contains an updated list of the hundred questions and answers that comprise
-                    the citizenship test. All questions and answers are taken from the U.S. citizenship
-                    and Immigration Services (USCIS) website. There are nine Q&A/Quiz pairs, plus a stand
-                    alone page of questions for which there is not a quiz. Hover over the green menu button
-                    in the upper left-hand corner to access the Q&A pages and quizzes. Study the questions
-                    and answers. Then, click on the associated quiz. It is that simple! (It's suggested that
-                    you attempt Quiz One first, in order to view the chronology of the quiz.)
-                </p>
+                <h3 id='home-banner'>Instructions and other useful information:</h3>
+                <ul id='unordered-list'>
+                    <li class='list-items'></li>
+                </ul>
 
             </div>
         </div>
