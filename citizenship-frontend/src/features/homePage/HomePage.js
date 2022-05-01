b00d1130654d4c0997169bfbc8dd48854dc49b0a
McCray <tubach75@hotmail.com>
2022-05-01 09:33:38 -0400

    turned homepage unordered list to ordered

--- citizenship-frontend/src/features/homePage/HomePage.js
+++ citizenship-frontend/src/features/homePage/HomePage.js
@@ -10,13 +10,13 @@ export const HomePage = () => {
             </div>
             <div id='home-instructions'>
                 <h3 id='home-banner'>Instructions and other useful information:</h3>
-                <ul id='unordered-list'>
+                <ol id='unordered-list'>
                     <li class='list-items'>Hover over the green Menu button.</li>
                     <li class='list-items'>Select a Q&A, and study the questions and answers.</li>
                     <li class='list-items'>Select the accompanying quiz.</li>
                     <li class='list-items'>Repeat as necessary.</li>
                     <li class='list-items'>Don't forget to study the "Remaining Questions!"</li>
-                </ul>
+                </ol>
 
             </div>
         </div>
