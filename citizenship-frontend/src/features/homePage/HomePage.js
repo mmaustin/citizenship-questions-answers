0e45bd0ebb082cd2de4a71f07ccb55b9bdd334c1
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2022-05-13 13:44:36 -0400

    added misc stylings to homepage component

--- citizenship-frontend/src/features/homePage/HomePage.js
+++ citizenship-frontend/src/features/homePage/HomePage.js
@@ -8,16 +8,14 @@ export const HomePage = () => {
                 <h3 id='welcome-start'>Let's Get Started!</h3>
             </div>
             <div id='home-instructions'>
-                <h3 id='home-banner'>Instructions and other useful information:</h3>
                 <ul id='unordered-list'>
                     <li className='list-items'>HOVER OVER THE GREEN MENU BUTTON.</li>
-                    <li className='list-items'>SELECT A Q&A.</li>
+                    <li className='list-items'>SELECT A Q&A FROM THE DROPDOWN.</li>
                     <li className='list-items'>STUDY THE QUESTIONS AND ANSWERS.</li>
                     <li className='list-items'>TAKE THE ACCOMPANYING QUIZ.</li>
                     <li className='list-items'>STUDY THE "REMAINING QUESTIONS!"</li>
                     <li className='list-items'>REPEAT AS NECESSARY.</li>
                 </ul>
-
             </div>
         </div>
     )
