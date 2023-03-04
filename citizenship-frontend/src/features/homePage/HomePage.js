46686719c5c6412d56c7ac6708d9b24b38bc1732
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2023-03-04 10:35:01 -0500

    added a link to the USCIS official site and a note to go to that site before taking the actual test

--- citizenship-frontend/src/features/homePage/HomePage.js
+++ citizenship-frontend/src/features/homePage/HomePage.js
@@ -17,6 +17,12 @@ export const HomePage = () => {
                     <li className='list-items'>REPEAT AS NECESSARY.</li>
                 </ul>
             </div>
+            <h3 style={{color: 'red'}} >Please Read Below</h3>
+            <p>This site is meant to be an aid for those who wish to avoid the clutter of the official U.S.
+                Citizenship and Immigration Services(USCIS) site.  It is highly recommended, however, that you visit
+                the official site, before taking the civics portion of the citizenship test.  There may be 
+                important updates to questions that I have yet to incorporate: <a rel="noopener noreferrer" target="_blank" href='https://www.uscis.gov/citizenship' >USCIS</a>
+            </p>
         </div>
     )
 }
